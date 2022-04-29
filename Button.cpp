294052c4ee9fcc87b549edbf65a927bec27a6023
sahar156 <102778309+sahar156@users.noreply.github.com>
2022-04-29 13:27:17 +0200

    snake and ladder game
    button code

---
+++ Button.cpp
@@ -0,0 +1,46 @@
+#include "Button.h"
+
+Button::Button(sf::Vector2f button_size, sf::Vector2f button_position, int charSize, sf::Color button_background, sf::Color button_text_color)
+{
+	this->button.setSize(button_size);
+	this->button.setFillColor(button_background);
+	this->button.setPosition(button_position);
+	this->button_width = button_size.x;
+	this->button_hight = button_size.y;
+	if (!this->font.loadFromFile("E:/fonts/Open_Sans/OpenSans-SemiBoldItalic.ttf"))
+		printf("Error!!\n");
+	this->text.setFont(this->font);
+	this->text.setCharacterSize(charSize);
+	this->text.setPosition(this->button.getPosition().x / 2.0f - text.getGlobalBounds().width / 2.0f,
+						   this->button.getPosition().y / 2.0f - text.getGlobalBounds().height / 2.0f);
+}
+void Button::setBackColor(sf::Color color)
+{
+	this->button.setFillColor(color);
+}
+void Button::drawTo(sf::RenderWindow* window)
+{
+	window->draw(this->button);
+	window->draw(this->text);
+}
+bool Button::isMouseOver(sf::RenderWindow* window)
+{
+	int mouseX = sf::Mouse::getPosition(*window).x;
+	int mouseY = sf::Mouse::getPosition(*window).y;
+	int btnPosX = button.getPosition().x;
+	int btnPosY = button.getPosition().y;
+	int btnxPosWidth = button.getPosition().x + this->button_width;
+	int btnyPosHeight = button.getPosition().y + this->button_hight;
+	if (mouseX < btnxPosWidth && mouseX > btnPosX && mouseY < btnyPosHeight && mouseY > btnPosY)
+		return true;
+	return false;
+}
+bool Button::isClicked(sf::RenderWindow* window)
+{
+	if (this->isMouseOver(window))
+	{
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+			return true;
+	}
+	return false;
+}
